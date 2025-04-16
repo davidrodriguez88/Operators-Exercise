83f8ceb5679d394e885fcbbd6a9e3ff4d756b66c
davidrodriguez88 <davidxrodriguez@me.com>
2025-04-16 12:37:08 -0400

    completed exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static double AreaofCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void PrintAreaofCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {Math.Round(AreaofCircle(radius), 2)}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtraction = a - b;
+            int mult = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            PrintAreaofCircle();
+            
         }
+
+
     }
 }
